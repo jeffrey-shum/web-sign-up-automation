610bc6a7c07096e74e9fd2f5b1017fb03522e24e
Jeff Shum <jeffshum1@gmail.com>
2020-10-15 14:06:45 +0700

    Make check_in and sign_up files and initial work on sign_up file

---
+++ check_in.py
@@ -0,0 +1,10 @@
+# URL for calendar: https://crossfitamatak.sites.zenplanner.com/calendar.cfm
+# URL again with arguments: https://crossfitamatak.sites.zenplanner.com/calendar.cfm?DATE=2020%2D10%2D14&VIEW=month&PERSONID=E808BD6B%2DFF80%2D4E7A%2D91EF%2D8A840CF1CC4C
+
+from selenium import webdriver
+PATH = "/cfamatak-automation/chromedriver"
+
+driver = webdriver.Chrome(PATH)
+driver.get("https://google.com")
+print(driver.title)
+driver.quit()
