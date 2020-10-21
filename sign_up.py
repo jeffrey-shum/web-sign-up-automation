c2055f3cb9457c82d1f42f5b3839e9dcc2d554dd
Jeff Shum <jeffshum1@gmail.com>
2020-10-21 15:03:47 +0700

    Add the majority of code needed to automate sign up

--- sign_up.py
+++ sign_up.py
@@ -1,58 +1,81 @@
 # sign_up.py
 
-# URL for calendar: https://crossfitamatak.sites.zenplanner.com/calendar.cfm
-# URL for calendar set to month view: https://crossfitamatak.sites.zenplanner.com/calendar.cfm?DATE=2020%2D10%2D15&VIEW=month
-# URL again with arguments: https://crossfitamatak.sites.zenplanner.com/calendar.cfm?DATE=2020%2D10%2D14&VIEW=month&PERSONID=E808BD6B%2DFF80%2D4E7A%2D91EF%2D8A840CF1CC4C
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from datetime import timedelta, datetime
-from time import sleep
 
+# Creating date variables:
 today = datetime.today()
 seven_days_from_today = today + timedelta(days=7)
 date_string = seven_days_from_today.strftime("%A %B %e")
+class_time = "6:15 AM - 7:00 AM"
+#TODO: delete after testing:
+    #class_time = "7:45 AM - 8:30 AM"
 year = str(seven_days_from_today.year)
 month = str(seven_days_from_today.month)
 day = str(seven_days_from_today.day)
 day_var = today.weekday() + 3 #this is used to compose the xpath to the sign-up link depending on what day of the week it is
 
-login_url = "https://crossfitamatak.sites.zenplanner.com/login.cfm"
-my_person_id = "personId=E808BD6B-FF80-4E7A-91EF-8A840CF1CC4C"
+# Processing data from text file:
+with open("/Users/jeff/cfamatak_automation/data.txt") as file:
+    lines = file.readlines()
+    lst = []
+    for line in lines:
+        line = line.strip().split(',')
+        lst.append(line)
+    login_url = lst[0][1]
+    calendar_url = lst[1][1] + f"{year}-{month}-{day}"
+
+# Creating variables for working with Chrome:
 xpath_date_sensitive = f"/html/body/div/table/tbody/tr/td[2]/table[2]/tbody/tr/td[{day_var}]/div/div[3]"
-#xpath_tues = "/html/body/div/table/tbody/tr/td[2]/table[2]/tbody/tr/td[4]/div/div[3]"
-#xpath = "/html/body/div/table/tbody/tr/td/table/tbody/tr/td/div"
-#xpath2 = "/html/body/div/table/tbody/tr/td/table/tbody/tr" #this should return 2 elements
-url = f"https://crossfitamatak.sites.zenplanner.com/calendar.cfm?DATE={year}-{month}-{day}"
-#test_url = "https://crossfitamatak.sites.zenplanner.com/calendar.cfm?DATE=2020-10-15&VIEW=month"
+#TODO: delete after testing, this is just an example:
+    #xpath_date_sensitive = "/html/body/div/table/tbody/tr/td[2]/table[2]/tbody/tr/td[5]/div/div[6]" 
 PATH = "/Users/jeff/chromedriver"
 driver = webdriver.Chrome(PATH)
 
 def login(login_url):   
     driver.get(login_url)
     elem = driver.find_element_by_id("idUsername")
-    elem.send_keys("username")
+    elem.send_keys(lst[2][0]) #TODO: add functionality to sign up for multiple users
     elem = driver.find_element_by_id("idPassword")
-    elem.send_keys("password")
+    elem.send_keys(lst[2][1])
     elem.send_keys(Keys.RETURN)
 
-def sign_up(url, xpath, sign_up_date):
-    driver.get(url)
+def sign_up(calendar_url, xpath_date_sensitive):
+    driver.get(calendar_url)
     try:
-        elem = driver.find_element_by_xpath(xpath)
+        elem = WebDriverWait(driver, 5).until(
+            expected_conditions.element_to_be_clickable((By.XPATH, xpath_date_sensitive))
+        )
         elem.click()
 
-        print(date_string) 
-        if date_string in driver.page_source:
-            print("found date string in the page source!")
-            elem = WebDriverWait(driver, 5).until(
-                expected_conditions.element_to_be_clickable((By.ID, "reserve_1"))
-            )
-            elem.click()
-            print("Attempted to find and click reserve button")
+        # Checks whether the member is already registered for the class:
+        if "Jeff Shum is registered for this class" in driver.page_source:             
+            print(f"Jeff Shum is already registered for {class_time} Bootcamp class on {date_string}")
+        # Checks whether browser is on the correct page:
+        elif (date_string in driver.page_source) and (class_time in driver.page_source): 
+            try:
+                elem = WebDriverWait(driver, 5).until(
+                    expected_conditions.element_to_be_clickable((By.ID, "reserve_1"))
+                )
+                elem.click()
+                if "Jeff Shum is registered for this class" in driver.page_source:
+                    print(f"Jeff Shum was successfully registered for {class_time} Bootcamp class on {date_string}.")
+                else:
+                    print("Something went wrong, manually check to see whether the member is signed up for the class.")
+            except:
+                print("Something went wrong, manually check to see whether the member is signed up for the class.")
+        else:
+            print("There was a problem. It's likely that the script navigated to the wrong sign-up link.")
     finally:
         driver.quit()
 
+def main():
+    login(login_url)
+    sign_up(calendar_url, xpath_date_sensitive)
+
+if __name__ == "__main__":
+    main()
